e6b07dd9329c9014cd6e71f5be30695a6d61cdfd
t-ryles <tajhakeem.ryles@gmail.com>
2021-02-06 12:19:33 -0600

    Intersectino Observer for sections
    Added the intersection observer fucntion for all sections, which add the 'your-active-class' to each section once they enter the view port.
    
    -Need to figure out how to remove class once out of view port.

--- UdacityLandingPageFile/JS/app.js
+++ UdacityLandingPageFile/JS/app.js
@@ -63,28 +63,93 @@ sections.forEach(section => {
     li.appendChild(li_aboutText);
          //Add to HTMl
     navBarList.appendChild(li);
-    console.log(navBarList)
 })
 
 // Scroll to section on link click
-var section = document.querySelectorAll('section');
+var observerSections = document.querySelectorAll('section');
+const options = {};
 
-var optionsSection = {
-       
-};
+const observer = new IntersectionObserver(function(entries, observer){
+       entries.forEach(entry => {
+             if(entry.isIntersecting){
+              entry.target.classList.add('your-active-class')};
+              console.log(entry.target);
+             })
 
-function scrollToSection(observerSection, event) {
-    observerSection = document.querySelectorAll("section");
-    event = new IntersectionObserver(function(entries, observerSection){
-        entries.forEach(entry => {
-               if(entry.isIntersecting){
-                      document.querySelectorAll('section').classList.add('your-active-class')
-               } else {
-                      documentquerySelectorAll('section').classList.remove('your-active-class')
-               };
-        });
- }, optionsSection);
- 
- observerSection.observe(section);
-}
-var clickEvent = document.getElementsByTagName('section').addEventListener('click', scrollToSection)
+}, options);
+
+observerSections.forEach(observerSection => {
+       observer.observe(observerSection);
+});
+
+
+/*  Intersection Observer
+
+--Selecting the area for the function to watch--
+       -Watching one section
+const sectionOne = document.querySelector('.section1');
+       -Watching all the sections
+const section = document.querySelectorAll('section');
+
+--Setting object for options.
+const options = {};
+       --There are three optins:
+       root: (default is null and the VP, optional to set)
+       threshold: 0-1 scale (Default is 0)
+       rootMargin: (Works just like regular margin in CSS, has to have px or % value)
+
+--Creating the observer--
+const observer = new Intersectionobserver(callback, options);
+
+--Callback is going to be a function.
+--The funstion is going to call two things(function(entries, observer))
+
+const observer = new Intersectionobserver(function(entries, observer){
+       --function goes here--
+}, options);
+
+--For each entrie we want to do something.
+
+const observer = new Intersectionobserver(function(entries, observer){
+       entries.forEach(entry => {
+              console.log(entry);
+       })
+}, options);
+
+--After creating the observer we have to use it
+
+const observer = new Intersectionobserver(function(entries, observer){
+       entries.forEach(entry => {
+              console.log(entry); //Adding .target will just give info about the target.
+       })
+}, options);
+
+--Tell the observer to observe sectionOne
+observer.observe(sectionOne);
+
+--Is intersecting will fire when the element being observerd goes on or off the page.
+
+--Editing to watch all the sections
+
+const observer = new Intersectionobserver(function(entries, observer){
+       entries.forEach(entry => { 
+              if (!entry.isIntersecting){ //If the section is not intersecting ignore code
+                     return;
+              }
+              console.log(entry.target); //Adding .target will just give info about the target.
+              entry.target.classList.toggle('className');
+              observer.unobserve(entry.target); //To stop observer the target
+       })
+}, options);
+
+observer.observe(section);
+--Will produce error, Sections is a Node list. Can't have one observer on the whole list.
+
+--Creating a forEach loop to combat the issue
+
+sections.forEach(section => {
+       observer.observe(section);
+});
+
+--For each section inside of the sectons, observer that section.
+*/
