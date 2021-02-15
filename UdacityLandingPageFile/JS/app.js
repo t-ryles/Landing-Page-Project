600170965d8ed393a68cc09458d3df873bfac4bc
t-ryles <tajhakeem.ryles@gmail.com>
2021-02-14 22:03:16 -0600

    Smooth Scroll
    Found code for dynamic smooth scroll to event...need to figure out how to get it functional.

--- UdacityLandingPageFile/JS/app.js
+++ UdacityLandingPageFile/JS/app.js
@@ -100,3 +100,19 @@ function onClick() {
 
 
 // Scroll to section on link click
+
+$(function() {
+       $('a[href*=#]:not([href=#])').click(function() {
+           if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+
+               var target = $(this.hash);
+               target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+               if (target.length) {
+                   $('html,body').animate({
+                       scrollTop: target.offset().top
+                   }, 1000);
+                   return false;
+               }
+           }
+       });
+});
